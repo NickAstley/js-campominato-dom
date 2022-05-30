a1ef32fc573058cd55565f9e247f1b0a08905718
NickAstley <andreanicastro1994@gmail.com>
2022-05-30 18:24:34 +0200

    Completed bonus

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 /* Il computer deve generare 16 numeri casuali compresi nel range della griglia: le bombe.
 I numeri nella lista delle bombe non possono essere duplicati.
 In seguito l’utente clicca su una cella:
-se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
-altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+se il numero è presente nella lista dei numeri generati 
+    - abbiamo calpestato una bomba 
+    - la cella si colora di rosso e la partita termina
+altrimenti 
+    - la cella cliccata si colora di azzurro
+    - l’utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 BONUS:
@@ -30,15 +34,19 @@ submitButton.addEventListener("click", function() {
     gameOver = false;
     // Prendo il valore del select
     const selectedDifficulty = document.getElementById("difficultySelector").value;
-    if (selectedDifficulty === "easy") {
+    switch (selectedDifficulty) {
         // Se è stata scelta la difficoltà facile, creo una griglia 10*10
-        createGrid(10, 10);
-    } else if (selectedDifficulty === "medium") {
+        case "easy":
+            createGrid(10, 10);
+            break;
         // Se è stata scelta la difficoltà media, creo una griglia 9*9
-        createGrid(9, 9);
-    } else if (selectedDifficulty === "hard") {
+        case "medium":
+            createGrid(9, 9);
+            break;
         // Se è stata scelta la difficoltà difficile, creo una griglia 7*7
-        createGrid(7, 7);
+        case "hard":
+            createGrid(7, 7);
+            break;
     }
 });
 
@@ -56,7 +64,7 @@ function createGrid(rows, cols) {
     // Creo ogni cella usando un ciclo
     for (let i = 1; i <= cellsTotal; i++) {
         // Aggiungo la cella al container
-        gridContainer.append(createCell(i, bombsList));
+        gridContainer.append(createCell(i, bombsList, cellsTotal));
     }
 }
 
@@ -75,7 +83,7 @@ function createBombsList (cellsTotal) {
     return bombsList;
 }
 
-function createCell(counter, bombsList) {
+function createCell(counter, bombsList, cellsTotal) {
     // Creo la cella
     const cell = document.createElement("div");
     // Le fornisco una classe
@@ -84,24 +92,50 @@ function createCell(counter, bombsList) {
     cell.innerHTML = `<span>${counter}</span>`;
     cell.dataset.index = counter;
     // Le aggiungo un EventListener che la colora al click
-    addOnCellClick(cell, bombsList);
+    addOnCellClick(cell, bombsList, cellsTotal);
     // Ritorno la cella
     return cell;
 }
 
-function addOnCellClick(cell, bombsList) {
+function addOnCellClick(cell, bombsList, cellsTotal) {
+    // Aggiungo un EventListener per il left-click
     cell.addEventListener("click", function () {
+        // Se la partita è finita, al click non deve succedere nulla
         if (gameOver) {
             return;
         }
+        // Rimuovo l'eventuale classe che segnala il pericolo
+        this.classList.remove("danger");
         if (bombsList.includes(+this.dataset.index)){
-            this.classList.add("bomb");
+            // Se la cella è una bomba, la partita finisce e le bombe si colorano di rosso
+            for (let i = 0; i < bombsList.length; i++) {
+                document.querySelector(`[data-index="` + bombsList[i] + `"]`).classList.add("bomb");
+            }
             gameOver = true;
-            alert("Hai perso! Punteggio:" + userScore);
+            // Viene mostrato un messaggio di fine partita con il punteggio finale
+            alert("Hai perso! Punteggio: " + userScore);
         } else {
+            // Se la cella non è una bomba, si colora di azzurro e il punteggio aumenta
             this.classList.add("safe");
             userScore++;
+            // Se il punteggio ha raggiunto il massimo, la partita finisce con una vittoria
+            if (userScore === cellsTotal - bombsList.length) {
+                gameOver = true;
+                alert("Complimenti, hai vinto! Punteggio: " + userScore);
+            }
         }
+    });
+
+    // Aggiungo un EventListener per il right-click
+    cell.addEventListener("contextmenu", function (e) {
+        // Prevengo il comportamento di base del right-click
+        e.preventDefault();
+        // Se la partita è finita, al click non deve succedere nulla
+        if (gameOver) {
+            return;
+        }
+        // Al right-addOnCellClick, coloro la cella per segnalarla come pericolosa
+        this.classList.add("danger");
     });
 }
 
